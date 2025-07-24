7f4271103131d3e152cc6d2bf9b5641647b0faab
deboschr <debospencher@gmail.com>
2025-07-25 00:26:01 +0700

    Update Application.java

--- src/main/java/com/unpar/brokenlinkchecker/Application.java
+++ src/main/java/com/unpar/brokenlinkchecker/Application.java
@@ -3,18 +3,23 @@ package com.unpar.brokenlinkchecker;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        // Buat stage tanpa title bar bawaan OS
+//        stage.initStyle(StageStyle.UNDECORATED);
+
         stage.setTitle("BrokenLink Checker");
         stage.setScene(scene);
 
-        stage.setMinWidth(900);   // lebar minimum
-        stage.setMinHeight(600);  // tinggi minimum
-        stage.setMaximized(true); // tampil fullscreen saat mulai
+        stage.setMinWidth(900);
+        stage.setMinHeight(600);
+        stage.setMaximized(true);  // Atau bisa dihapus kalau tidak ingin fullscreen
 
         stage.show();
     }
